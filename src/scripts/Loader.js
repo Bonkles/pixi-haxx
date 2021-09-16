4048e69294548604f33b9da3d3e67e0eccc8f267
Benjamin Clark <clarkben@fb.com>
2021-09-16 10:11:59 -0400

    Fix image resource loading of background.

--- src/scripts/Loader.js
+++ src/scripts/Loader.js
@@ -1,14 +1,15 @@
+const bg = require('../sprites/bg.png')
 class Loader {
   constructor (loader) {
-    this.loader = loader; 
+    this.loader = loader;
   }
 
   preload() {
-    this.loader.add('background', '../src/sprites/bg.png');
+    this.loader.add('background', bg);
     this.loader.load((loader, resources) => {
       console.log(resources);
     })
   }
 }
 
-export default Loader; 
+export default Loader;
